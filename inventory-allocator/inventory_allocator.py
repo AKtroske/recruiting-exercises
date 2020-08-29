16d9a5507dcedefde73c3f6e78b126df2270401a
Akahi Troske <akahitroske@gmail.com>
2020-08-29 16:14:59 -0700

    Added order/warehouse sanitization with tests.

--- inventory-allocator/inventory_allocator.py
+++ inventory-allocator/inventory_allocator.py
@@ -2,7 +2,8 @@ class InventoryAllocator:
 
     def process_order(self, order, warehouses):
         """
-            Process order by finding the cheapest shipment given order and inventory variables.
+            Process order by finding the cheapest shipment given order and inventory variables. Items are filled as passed in.
+            For each item, iterate through warehouses until order is 0 (filled) or all warehouses checked (order can't be filled).
 
             Parameters
             ----------
@@ -17,7 +18,12 @@ class InventoryAllocator:
                 shipment: list[dict] - [{name : { item: quanitity, ... }}, ...]
         """
 
-        if not self.validate_order(order):
+        order, warehouses = self.remove_empty_items(order, warehouses)
+
+        if len(order) == 0:
+            return []
+
+        if len(warehouses) == 0:
             return []
 
         final_shipment = []
@@ -54,37 +60,30 @@ class InventoryAllocator:
 
                 i += 1
 
+            # Check if order for specific item has been satisfied
+            if i == len(warehouses) and order[item] > 0:
+                return []
+
         print(final_shipment)
         return final_shipment
 
-    def validate_order(self, order):
+    def remove_empty_items(self, order, warehouses):
         """
+        Checks and removes ordered items that have quantity greater than zero using list, dict comprehension
+
         Parameters
         ----------
             order: dict - { item : quanitity, ... }
                 A dict. of items with corresponding quantities to be filled
-
         Returns
         ----------
-            valid - bool describing validity of order
+            cleaned: dict - { item : quanitity, ...}
+                All items have quantity > 0
         """
 
-        def remove_empty_items(order):
-            """
-            Checks and removes ordered items that have quantity greater than zero
-            Parameters
-            ----------
-                order: dict - { item : quanitity, ... }
-                    A dict. of items with corresponding quantities to be filled
-            Returns
-            ----------
-                cleaned: dict - { item : quanitity, ...}
-                    All items have quantity > 0
-            """
+        cleaned_order = {k:v for (k, v) in order.items() if v > 0}
+        cleaned_warehouses =  [{'name':warehouse['name'], 'inventory':{k:v for k, v in warehouse['inventory'].items() if v > 0}} for warehouse in warehouses]
+        return cleaned_order, cleaned_warehouses
 
-            cleaned = [k for k, v in order.items() if v > 0]
-            return cleaned
-
-        order = remove_empty_items(order)
-
-        return True
+if __name__ == '__main__':
+    ia = InventoryAllocator()
--- inventory-allocator/test_inventory_allocator.py
+++ inventory-allocator/test_inventory_allocator.py
@@ -7,16 +7,34 @@ ia = InventoryAllocator()
 def test_1():
     print("\n\nRunning tests...\n--------------------------------")
     print("Test 1 ...")
-    assert [{'warehouse36': {'aa': 2}}] == ia.process_order({'aa':2}, [{'name':'warehouse36','inventory':{'a':3, 'aa':3, 'aaa': 3}}])
+    assert [
+        {'w1': {'a': 2}}
+    ] == ia.process_order({'a':2}, [
+        {'name':'w1','inventory':{'a':3, 'aa':3, 'aaa': 3}}
+    ])
 
 def test_2():
-    print("Test 2 ...")
+    print("\nTest 2 ...")
     assert [
-        {'abc': {'apple': 6, 'orange': 6}},
-        {'ghi': {'apple': 4}},
-        {'def': {'orange': 1, 'banana': 4}}
+        {'w1': {'apple': 6, 'orange': 6}},
+        {'w3': {'apple': 4}},
+        {'w2': {'orange': 1, 'banana': 4}}
     ] == ia.process_order({'apple': 10, 'orange': 7, 'banana': 4}, [
-        {'name': 'abc', 'inventory': {'apple': 6, 'orange': 6}},
-        {'name': 'def', 'inventory': {'banana': 4, 'orange': 1}},
-        {'name': 'ghi', 'inventory': {'banana': 3, 'orange': 3, 'apple': 5}}
+        {'name': 'w1', 'inventory': {'apple': 6, 'orange': 6}},
+        {'name': 'w2', 'inventory': {'banana': 4, 'orange': 1}},
+        {'name': 'w3', 'inventory': {'banana': 3, 'orange': 3, 'apple': 5}}
     ])
+
+# Test invalid order
+def test_3():
+    print("\nTest 3 ... Order cannot be filled")
+    assert [] == ia.process_order({'apple':5}, [
+        {'name':'w1', 'inventory':{'apple':2}},
+        {'name':'w2', 'inventory':{'apple':2}}
+    ])
+
+# Test remove empty-items
+def test_4():
+    print("\nTest 4 ... Test removing empty items")
+    co, cv = ia.remove_empty_items({'apple':-1}, [{'name':'a', 'inventory':{'apple':-10, 'banana':-15, 'choco':10}}, {'name':'b', 'inventory':{'b_apple':-10, 'b_banana':-15, 'b_choco':10}}])
+    assert co == {} and cv == [{'name': 'a', 'inventory': {'choco': 10}}, {'name': 'b', 'inventory': {'b_choco': 10}}]
